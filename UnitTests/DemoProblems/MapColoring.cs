6f8eb9c4cfbd940d5ddde35f9198ef944779b113
Scott Mitchell <scott.mitchell@autodesk.com>
2019-02-06 16:05:48 -0500

    Added Map Coloring example

---
+++ UnitTests/DemoProblems/MapColoring.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csp.Constraints;
+
+namespace Csp.UnitTests.DemoProblems
+{
+    class MapColoring
+    {
+        public static Problem<string, int> CreateProblem(int k = 3)
+        {
+            var colors = Enumerable.Range(1, k).ToList();
+
+            var adjacencies = new Dictionary<string, List<string>> {
+                { "Western Australia", new List<string> { "Northern Territory", "South Australia" } },
+                { "Northern Territory", new List<string> { "Western Australia", "South Australia", "Queensland" } },
+                { "New South Wales", new List<string> { "Queensland", "South Australia", "Victoria" } },
+                { "Queensland", new List<string> { "Northern Territory", "South Australia", "New South Wales" } },
+                { "South Australia", new List<string> { "Western Australia", "Northern Territory", "New South Wales", "Queensland", "Victoria" } },
+                { "Victoria", new List<string> { "New South Wales", "South Australia" } },
+                { "Tasmania", new List<string> { } }
+            };
+
+            var territories = adjacencies.Keys.ToList();
+
+            var variables = territories.Select(t => new Variable<string, int>(t, colors)).ToList(); ;
+
+            var constraints = new List<IConstraint<string, int>>();
+            for (int i = 0; i < territories.Count; i++)
+            {
+                var neighbors = adjacencies[territories[i]];
+                foreach (string n in neighbors)
+                {
+                    var neighborIndex = territories.IndexOf(n);
+                    constraints.Add(new AllDifferentConstraint<string, int>(new List<Variable<string, int>> { variables[i], variables[neighborIndex] } ));
+                }
+            }
+
+            return new Problem<string, int>(variables, constraints);
+        }
+    }
+}
---
+++ UnitTests/DemoProblems/TestMapColoring.cs
@@ -0,0 +1,49 @@
+﻿using Csp.UnitTests.DemoProblems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Csp.UnitTests.DemoProblems
+{
+    class TestMapColoring
+    {
+        [Test]
+        public void Solve_BacktrackSolver()
+        {
+            var problem = MapColoring.CreateProblem();
+            var solver = new RecursiveBacktrackSolver<string, int>();
+            var solution = solver.Solve(problem, CancellationToken.None);
+
+            var assigned = new Dictionary<string, int>();
+            foreach (Variable<string, int> v in problem.Variables)
+            {
+                assigned[v.UserObject] = solution.GetValue(v);
+            }
+
+
+            var adjacencies = new Dictionary<string, List<string>> {
+                { "Western Australia", new List<string> { "Northern Territory", "South Australia" } },
+                { "Northern Territory", new List<string> { "Western Australia", "South Australia", "Queensland" } },
+                { "New South Wales", new List<string> { "Queensland", "South Australia", "Victoria" } },
+                { "Queensland", new List<string> { "Northern Territory", "South Australia", "New South Wales" } },
+                { "South Australia", new List<string> { "Western Australia", "Northern Territory", "New South Wales", "Queensland", "Victoria" } },
+                { "Victoria", new List<string> { "New South Wales", "South Australia" } },
+                { "Tasmania", new List<string> { } }
+            };
+
+            
+            foreach (string t in adjacencies.Keys)
+            {
+                var neighbors = adjacencies[t];
+                foreach (string n in neighbors)
+                {
+                    Assert.AreNotEqual(assigned[t], assigned[n]);
+                }
+            }
+        }
+    }
+}
